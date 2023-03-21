ca6281e754723825daf371c9d1bcf644b6f67a26
Anthony Romano <dentlabpa@gmail.com>
2023-03-21 16:05:01 -0400

    add flask-wtf and create sign up form

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
 from app import app
 from flask import render_template
+from fake_data import posts
+from app.forms import SignUpForm
 
 
 @app.route('/')
 def index(): 
-    return render_template('index.html')
+    return render_template('index.html', posts=posts, logged_in=False)
 
 
 @app.route('/signup')
 def signup(): 
-    return render_template('signup.html')
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
     
 
 @app.route('/login')
