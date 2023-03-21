c3a3ea1ce88146d26cf9d155092922c6406558bd
Anthony Romano <dentlabpa@gmail.com>
2023-03-21 13:19:05 -0400

    updated everything

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
-def hello_world(): 
-    return 'Hello World!'
+def index(): 
+    return render_template('index.html')
 
 
 @app.route('/test')
